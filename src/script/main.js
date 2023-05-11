47cb07901efce934b0c406ca092d656cb38b5c19
KrzysiekLu <krzysztof.luuczak@gmail.com>
2023-05-11 20:39:11 +0200

    draw and place img

--- src/script/main.js
+++ src/script/main.js
@@ -1,12 +1,26 @@
 const lvlBTNs = document.querySelectorAll(".game_level button");
 const board = document.querySelector(".game__board");
 
-const generateBoard = (numOfRows) => {
+const fetchImg = (numOfRows) => {
+  let imgArr = [];
+  for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < (numOfRows * numOfRows) / 2; i++) {
+      const img = document.createElement("img");
+      img.classList.add("tile__img");
+      img.src = `./src/images/img-${i}.png`;
+      imgArr.push(img);
+    }
+  }
+  const shuffledArray = imgArr.sort((a, b) => 0.5 - Math.random());
+  return shuffledArray;
+};
+
+const generateBoard = (numOfRows, imgArr) => {
   board.innerHTML = "";
   for (let i = 0; i < numOfRows * numOfRows; i++) {
     const tile = document.createElement("div");
     tile.classList.add("tile");
-    tile.textContent = i;
+    tile.appendChild(imgArr[i]);
     board.style.setProperty(
       "grid-template-columns",
       `repeat(${numOfRows}, 1fr)`
@@ -19,6 +33,6 @@ const generateBoard = (numOfRows) => {
 
 lvlBTNs.forEach((btn) =>
   btn.addEventListener("click", (e) => {
-    generateBoard(e.target.dataset.row);
+    generateBoard(e.target.dataset.row, fetchImg(e.target.dataset.row));
   })
 );
