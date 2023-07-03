e4a19212ce13a29ab7590dcc60113ae8cea83de8
KrzysiekLu <krzysztof.luuczak@gmail.com>
2023-07-03 21:58:46 +0200

    prevent multi click & refactoring

--- gameLogick/main.js
+++ gameLogick/main.js
@@ -9,46 +9,67 @@ const controler = {
   scores: 0,
   movment: false,
 };
-tilesArr.forEach((tile) => {
-  const boardTile = document.createElement("div");
-  boardTile.textContent = tile;
-  boardTile.classList.add("board__tile");
-  board.insertAdjacentElement("afterbegin", boardTile);
-});
+const createBoard = () => {
+  tilesArr.forEach((tile) => {
+    const boardTile = document.createElement("div");
+    boardTile.textContent = tile;
+    boardTile.classList.add("board__tile");
+    board.insertAdjacentElement("afterbegin", boardTile);
+  });
+};
+const countMoves = () => {
+  ++controler.moves;
+};
+
+const checkPair = () => {
+  if (controler.movesValue[0].innerText != controler.movesValue[1].innerText) {
+    controler.movesValue.forEach((clickedElement) => {
+      setTimeout(() => {
+        clickedElement.classList.remove("clicked");
+      }, 1000);
+      controler.movesValue = [];
+    });
+  } else {
+    controler.scores++;
+    score.textContent = controler.scores;
+
+    if (controler.movesValue.length === tilesArr.length) {
+      endGeame();
+    }
+  }
+};
+
 const endGeame = () => {
   setTimeout(() => {
     board.classList.add("board--win");
     board.textContent = "You win !!";
   }, 800);
 };
-board.addEventListener("click", (e) => {
-  ++controler.moves;
-  console.log(controler.moves);
 
-  if (controler.moves <= 2) {
-    e.target.classList.add("clicked");
-    controler.movesValue.push(e.target);
-    console.log(controler.movesValue);
+const init = () => {
+  createBoard();
+  board.addEventListener("click", (e) => {
+    if (
+      controler.moves <= 2 &&
+      !controler.movment &&
+      e.target.classList.contains("board__tile") &&
+      !e.target.classList.contains("clicked")
+    ) {
+      countMoves();
+      e.target.classList.add("clicked");
+      console.log(controler.moves);
 
-    if (controler.moves === 2) {
-      if (
-        controler.movesValue[0].innerText != controler.movesValue[1].innerText
-      ) {
-        controler.movesValue.forEach((clickedElement) => {
-          setTimeout(() => {
-            clickedElement.classList.remove("clicked");
-          }, 1000);
-          controler.movesValue = [];
-        });
-      } else {
-        controler.scores++;
-        score.textContent = controler.scores;
-
-        if (controler.movesValue.length === tilesArr.length) {
-          endGeame();
-        }
+      console.log(e.target.classList.contains("clicked"));
+      controler.movesValue.push(e.target);
+      if (controler.moves === 2) {
+        controler.movment = true;
+        setTimeout(() => {
+          controler.movment = false;
+        }, 1000);
+        checkPair();
+        controler.moves = 0;
       }
-      controler.moves = 0;
     }
-  }
-});
+  });
+};
+init();
